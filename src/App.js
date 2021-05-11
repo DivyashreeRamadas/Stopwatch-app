39de087fe93b27c0757aead3efdadde1e716acde
DivyashreeRamadas <divya14bebo@gmail.com>
2021-05-11 23:29:39 +0530

    Delete unwanted files and add only app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+const [initialTime, setInitialTime] = useState(0);
+const [timerOn, setTimerOn] = useState(false);
+
+
+///////////////////////////////////////////////////
+useEffect(()=> {
+
+  let interval = null;
+
+  if(timerOn){
+    interval = setInterval(()=> {
+      setInitialTime(prevTime => prevTime + 10)
+      }, 10)
+  }else{
+    clearInterval(interval);
+  }
+
+  return () => clearInterval(interval);
+},[timerOn]);
+
+
+
+///////////////////////////////////////////////////////////
+
+// const getTimeString = () => {
+//   if(initialTime > 9)
+//     return initialTime;
+//   else
+//     return ("0"+initialTime)
+// }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      
+        <h1> Timer </h1>
+        <div className="timer">
+          <span> {("0" + Math.floor((initialTime/(1000*60*60)) % 24)).slice(-2)} :</span>
+          <span> {("0" + Math.floor((initialTime/(1000*60)) % 60)).slice(-2)} :</span>
+          <span> {("0" + Math.floor((initialTime/1000) % 60)).slice(-2)} :</span>
+          <span> {("0" + ((initialTime/10) % 100)).slice(-2)} </span>
+        </div>
+
+        <div>
+          <button onClick={()=> setTimerOn(true)} className="buttons"> Start </button>
+          <button onClick={()=> setTimerOn(false)} className="buttons"> Stop </button>
+          <button onClick={()=> setTimerOn(true)} className="buttons"> Resume </button>
+          <button onClick={()=> setInitialTime(0)} className="buttons"> Reset </button>
+        </div>
     </div>
   );
 }
